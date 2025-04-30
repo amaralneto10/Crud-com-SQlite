f16986ab2656fbba73440ad7fd09b95da168bc4b
Amaral Neto <alunoamaralneto@gmail.com>
2025-04-30 17:00:29 -0300

    feat: finish user crud

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,35 +6,54 @@ const prisma = new PrismaClient()
 // }
 
 export const getAllUsers = async (req, res) => {
-    const { name } = req.body
-    const user = await prisma.
-    res.status(200).json(name)
+    try {
+        const users = await prisma.user.findMany()
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(`Erro ao listar usuários: ${error.message}`)
+    }
 }
-
-// export const postCreateUsers = (req, res) => {
-//     const { nome, email } = req.body
-
-//     const newUsers = {
-//         nome,
-//         email
-//     }
-
-//     res.status(201).json(newUsers)
-// }
 
 export const postCreateUsers = async (req, res) => {
     try {
         const { name, email } = req.body
 
-    const newUsers = await prisma.user.create({
-        data: {
-            name,
-            email
-        }
+        const newUsers = await prisma.user.create({
+            data: {
+                name,
+                email
+            }
     })
 
     res.status(201).json(newUsers)
     } catch (error) {
         res.status(500).json({mensagem: `Erro ao criar usuário: ${error.message}`})
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params
+    const { name, email } = req.body
+
+    try {
+        const updateUsuario = await prisma.user.update({
+            where: { id: parseInt(id) },
+            data: { name, email }
+        })
+        res.status(200).json(updateUsuario)
+    } catch (error) {
+        res.status(400).json({mensagem: `Erro ao atualizar: ${error.message}`})
+    }
+}
+
+export const deleteUsers = async (req, res) => {
+        const { id } = req.params
+    try {
+        const deleteUser = await prisma.user.delete({
+            where: { id: Number(id) }
+        })
+        res.status(204)
+    } catch (error) {
+        res.status(400).json(`Erro em deletar: ${error.message}`)
     }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { getAllUsers, postCreateUsers } from '../controllers/userController.js'
+import { getAllUsers, postCreateUsers, deleteUsers, updateUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
 
 router.post('/',postCreateUsers )
+
+router.put('/:id', updateUser)
+
+router.delete('/:id', deleteUsers)
 
 export default router
